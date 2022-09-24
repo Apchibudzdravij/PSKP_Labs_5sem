efb330595260bb84ac162b9ff6aeeecd925dc1b0
secxndary <valdaitsevv@mail.ru>
2022-09-24 22:26:01 +0300

    Lab 3 v1.0 full lab

--- PSKP_Lab3/03-01.js
+++ PSKP_Lab3/03-01.js
@@ -1,3 +1,26 @@
 var http = require('http');
-var fs = require('fs');
+global.state = 'test';
+global.oldState = 'test';
 
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    response.end('<html><body><h1>' + state + '</h1></body></html>');
+}).listen(5000, () => console.log('Server running at localhost:5000'));
+
+
+var stdin = process.openStdin();
+stdin.addListener('data', function (cmd) 
+{
+    var arg = cmd.toString().trim();
+    if (arg === 'norm' || arg === 'test' || arg === 'stop' || arg === 'idle') 
+    {
+        oldState = state;
+        state = cmd.toString().trim();
+        console.log(oldState + ' --> ' + state);
+    }
+    else if (cmd.toString().trim() === 'exit') 
+        process.exit(0);
+    else 
+        console.log('Enter one of the commands: norm, stop, test, idle or exit');
+});
