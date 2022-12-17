6ec693e3e8690066fd7cd3556f4f686ecc481e26
secxndary <valdaitsevv@mail.ru>
2022-12-17 20:12:02 +0300

    Lab 13 — different ports

--- PSKP_Lab13/13-05.js
+++ PSKP_Lab13/13-05.js
@@ -1,3 +1,41 @@
+const { createServer } = require('http');
 const net = require('net');
-const HOST = '';
-const PORT = 4000;
+const HOST = '0.0.0.0';
+const PORT_4000 = 4000;
+const PORT_5000 = 5000;
+let sum = 0;
+let label = (pfx, port, socket) => { return `${pfx} ${socket.remoteAddress}: ${socket.remotePort} -> `; }
+let connections = new Map();
+
+let socketHandler = port => {
+    return socket => {
+        console.log(`[${port}] Client connected: ${socket.remoteAddress}:${socket.remotePort}`);
+
+        socket.on('data', data => {
+            sum += data.readInt32LE();
+            console.log(`[${port}] Server received from client: `, data, `sum = ${sum}`);
+        })
+
+        let buf = Buffer.alloc(4);
+        setTimeout(() => {
+            setInterval(() => {
+                buf.writeInt32LE(sum, 0);
+                socket.write(buf);
+            }, 5000);
+        }, 500);
+
+        socket.on('close', () => { console.log(`[${port}] Connection closed.`); })
+        socket.on('error', error => { console.log(`[ERROR] Client - ${port}: ${error.message}`); });
+    }
+}
+
+
+
+net.createServer(socketHandler(PORT_4000))
+    .listen(PORT_4000, HOST)
+    .on('listening', () => { console.log(`\nStarted server: ${HOST}:${PORT_4000}`) });
+
+
+net.createServer(socketHandler(PORT_5000))
+    .listen(PORT_5000, HOST)
+    .on('listening', () => { console.log(`Started server: ${HOST}:${PORT_5000}\n`) });
--- PSKP_Lab13/13-06.js
+++ PSKP_Lab13/13-06.js
@@ -1,3 +1,26 @@
 const net = require('net');
-const HOST = '';
-const PORT = 4000;
+const HOST = '127.0.0.1';
+const PORT = process.argv[2] ? process.argv[2] : 4000;
+let number = process.argv[3] ? process.argv[3] : 1;
+let client = new net.Socket();
+let buffer = new Buffer.alloc(4);
+
+
+setTimeout(() => {
+    client.connect(PORT, HOST, () => {
+        console.log(`\nConnected to server: ${client.remoteAddress}:${client.remotePort}\nPORT: ${PORT}\nNUMBER: ${number}`);
+
+        let intervalWriteNumber = setInterval(() => {
+            client.write((buffer.writeInt32LE(number, 0), buffer));
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(intervalWriteNumber);
+            client.end();
+        }, 21000);
+    });
+
+    client.on('data', data => { console.log(`Client received from server: ${data.readInt32LE()}`); });
+    client.on('close', () => { console.log('Connection closed.'); });
+    client.on('error', error => { console.log('[ERROR] ' + error.message); });
+}, 100);
