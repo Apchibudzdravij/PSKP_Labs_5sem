96eb607b526419136f76ac0bb5674ea96f05f344
secxndary <valdaitsevv@mail.ru>
2022-12-23 20:10:16 +0300

    Lab 14 added server

--- PSKP_Lab14/14-01.js
+++ PSKP_Lab14/14-01.js
@@ -1,65 +1,26 @@
-const sql = require('mssql');
+const http = require('http');
+const fs = require('fs');
 const DB = require('./m14')();
+let server = http.createServer();
+
+
+let http_handler = (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    
+    DB.getFaculties().then(records => {
+        res.end(JSON.stringify(records.recordset, null, 4))
+    });
+}
 
 
 
-DB.getFaculties().then(records => {
-    console.log(JSON.stringify(records.recordset, null, 4))
-});
+server.listen(5000, () => {
+    console.log('Server running at http://localhost:5000/\n');
+}).on('error', error => {
+    console.log('[ERROR] ', error.message);
+}).on('request', http_handler);
 
 
-
-
-// const pool = new sql.ConnectionPool(config, err => {
-//     if (err) console.log('[ERROR] Cannot connect database.', err.message);
-//     else {
-//         console.log('[OK] Connected to database.');
-//         // dbreq01(pool);
-//         // dbreq02(pool);
-//         // dbreq03(pool);
-//         let pool = DB.connectionPool;
-//         DB.getFaculties().then(records => {
-//             console.log(JSON.stringify(records.recordset, null, 4))
-//         });
-//     }
-// })
-
-
-
-
-
-// let dbreq01 = (pool) => { pool.request().query('select faculty, faculty_name from FACULTY', processing_result) };
-
-// let dbreq02 = (pool) => { pool.request().query('select faculty, pulpit, pulpit_name from PULPIT order by faculty', processing_result) };
-
-// let dbreq03 = (pool) => { pool.request().query('select teacher, teacher_name, pulpit from TEACHER order by pulpit', processing_result) };
-
-
-
-// sql.connect(config, err => {
-
-//     if (err) console.log('[ERROR] Cannot connect database.', err.message);
-//     else {
-//         console.log('[OK] Connected to database.');
-//         dbreq01();
-//         dbreq02();
-//         dbreq03();
-//     }
+// DB.getFaculties().then(records => {
+//     console.log(JSON.stringify(records.recordset, null, 4))
 // });
-
-
-
-// let processing_result = (err, result) => {
-//     if (err) console.log('processing result error', err.code, err.originalError.info.message);
-//     else {
-//         console.log('Количество строк: ', result.rowsAffected[0]);
-//         for (let i = 0; i < result.rowsAffected[0]; i++) {
-//             let str = '--';
-//             for (key in result.recordset[i]) {
-//                 str += ` ${key} = ${result.recordset[i][key]}`;
-//             }
-//             console.log(str);
-
-//         }
-//     }
-// }
