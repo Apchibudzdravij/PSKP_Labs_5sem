b49d987cce9e51f2cf8f0e5b45368769a0075689
secxndary <valdaitsevv@mail.ru>
2022-12-23 21:37:29 +0300

    Lab 14 get handler full

--- PSKP_Lab14/server/get_handler.js
+++ PSKP_Lab14/server/get_handler.js
@@ -1,47 +1,46 @@
 const url = require('url');
-const http = require('http');
+const error = require('./server_error_handler')();
 const DB = require('../database/db_module')();
 
+
+
 function Get_Handler(req, res) {
+
     let pathname = url.parse(req.url, true).pathname;
     console.log(pathname);
+    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
 
 
+    switch (pathname) {
 
-    if (pathname === '/api/faculties') {
-        DB.getFaculties().then(records => {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify(records.recordset), null, 4);
-        }).catch(error => { errorHandler(res, 2, error); });
-    }
+        case '/api/faculties': DB.getFaculties()
+            .then(records => { res.end(JSON.stringify(records.recordset, null, 4)); })
+            .catch(error => { error.handler(res, 411, error.message); });
+            break;
 
-    else if (pathname === '/api/pulpits') {
-        DB.getPulpits().then(records => {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify(records.recordset), null, 4);
-        }).catch(error => { errorHandler(res, 3, error); });
-    }
+        case '/api/pulpits': DB.getPulpits()
+            .then(records => { res.end(JSON.stringify(records.recordset, null, 4)); }).
+            catch(error => { error.handler(res, 412, error.message); });
+            break;
 
-    else if (pathname === '/api/subjects') {
-        DB.getSubjects().then(records => {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify(records.recordset), null, 4);
-        }).catch(error => { errorHandler(res, 4, error); });
-    }
+        case '/api/subjects': DB.getSubjects()
+            .then(records => { res.end(JSON.stringify(records.recordset, null, 4)); }).
+            catch(error => { error.handler(res, 413, error.message); });
+            break;
 
-    else if (pathname === '/api/auditoriumtypes') {
-        DB.getAuditoriumTypes().then(records => {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify(records.recordset), null, 4);
-        }).catch(error => { errorHandler(res, 5, error); });
-    }
+        case '/api/auditoriumtypes': DB.getAuditoriumTypes()
+            .then(records => { res.end(JSON.stringify(records.recordset, null, 4)); })
+            .catch(error => { error.handler(res, 414, error.message); });
+            break;
 
-    else if (pathname === '/api/auditoriums') {
-        DB.getAuditoriums().then(records => {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-            res.end(JSON.stringify(records.recordset), null, 4);
-        }).catch(error => { errorHandler(res, 6, error); });
+        case '/api/auditoriums': DB.getAuditoriums()
+            .then(records => { res.end(JSON.stringify(records.recordset, null, 4)); })
+            .catch(error => { error.handler(res, 415, error.message); });
+            break;
+
+        default: error.handler(res, 410, 'Incorrect URL'); break;
     }
 }
+
 
 module.exports = (req, res) => new Get_Handler(req, res);
